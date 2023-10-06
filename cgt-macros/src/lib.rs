6a8bd4698d42c19d3f9e58fadb990e9f46fb53a3
Maxime Ripard <mripard@kernel.org>
2023-10-06 09:56:22 +0200

    macros: Allow FD tests to have attributes and setup master and caps
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- cgt-macros/src/lib.rs
+++ cgt-macros/src/lib.rs
@@ -1,7 +1,8 @@
+use attribute_derive::FromAttr;
 use proc_macro::TokenStream;
 use proc_macro_error::{emit_error, proc_macro_error};
-use quote::quote;
-use syn::{parse::Parse, parse_macro_input, Expr, ItemFn, Token};
+use quote::{quote, ToTokens};
+use syn::{parse::Parse, parse_macro_input, Expr, Ident, ItemFn, Token};
 
 #[proc_macro]
 pub fn cgt_assert(item: TokenStream) -> TokenStream {
@@ -99,8 +100,57 @@ pub fn cgt_test(args: TokenStream, item: TokenStream) -> TokenStream {
     .into()
 }
 
+#[derive(Debug, FromAttr)]
+struct TestAttributes {
+    master: bool,
+
+    #[attribute(optional)]
+    capabilities: Vec<Ident>,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum ExplicitOption<T> {
+    Some(T),
+    None,
+}
+
+impl<T> From<Option<T>> for ExplicitOption<T> {
+    fn from(value: Option<T>) -> Self {
+        match value {
+            Some(v) => ExplicitOption::Some(v),
+            None => ExplicitOption::None,
+        }
+    }
+}
+
+impl<T: ToTokens> ToTokens for ExplicitOption<T> {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        if let ExplicitOption::Some(ref t) = *self {
+            tokens.extend(quote! { Some(#t) })
+        } else {
+            tokens.extend(quote! { None })
+        }
+    }
+}
+
 #[proc_macro_attribute]
-pub fn cgt_test_with_fd(_args: TokenStream, item: TokenStream) -> TokenStream {
+pub fn cgt_test_with_fd(args: TokenStream, item: TokenStream) -> TokenStream {
+    let attrs: TestAttributes = parse_macro_input!(args);
+    let master = attrs.master;
+
+    if attrs.capabilities.len() > 8 {
+        todo!();
+    }
+
+    let mut caps: [ExplicitOption<&Ident>; 8] = [ExplicitOption::None; 8];
+    for (idx, caps) in caps.iter_mut().enumerate() {
+        if idx >= 8 {
+            break;
+        }
+
+        *caps = attrs.capabilities.get(idx).into();
+    }
+
     let input = parse_macro_input!(item as ItemFn);
     let fn_ident = &input.sig.ident;
     let fn_name = fn_ident.to_string();
@@ -112,9 +162,9 @@ pub fn cgt_test_with_fd(_args: TokenStream, item: TokenStream) -> TokenStream {
             cgt_core::Test {
                 module_name: module_path!(),
                 test_name: #fn_name,
-                test_fn: TestFunction::WithFd(#fn_ident),
-                master: false,
-                client_capabilities: [None; 8],
+                test_fn: cgt_core::TestFunction::WithFd(#fn_ident),
+                master: #master,
+                client_capabilities: [#(#caps),*]
             }
         );
     }
